13bef0b5703636e6001b50266753a4d95eb6b6e3
Valeria N <v.567insp@gmail.com>
2025-07-10 20:40:16 +0300

    dark mode added

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,17 +7,17 @@ export default function Header() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className="fixed t-0 l-0 z-1000 bg-sky-700/50 p-7 w-full backdrop-blur-md dark:bg-gray-800">
+    <div className="fixed t-0 l-0 z-1000 bg-sky-700/50 p-7 w-full backdrop-blur-md dark:bg-gray-800/50">
       <ul className="flex justify-end gap-10 text-sky-950 text-lg font-semibold dark:text-white">
-        <li className="hover:text-sky-800">
+        <li className="hover:text-sky-800 hover:dark:text-sky-100">
           <Link to="/WeatherDisplay">Weather</Link>
         </li>
-        <li className="hover:text-sky-800">
+        <li className="hover:text-sky-800 hover:dark:text-sky-100">
           <Link to="/AirPolution">Air Polution</Link>
         </li>
         <button
           onClick={toggleTheme}
-          className="px-3 py-1 rounded-md bg-blue-500 text-white hover:bg-blue-600"
+          className="px-3 py-1 rounded-md bg-sky-700 text-white hover:bg-sky-600"
         >
           {theme === "light" ? <IoIosSunny /> : <FaMoon />}
         </button>
